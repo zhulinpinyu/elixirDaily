8ec50077b2541fe2cdc7ddadbec3b12c58057e1e
zhulinpinyu <lixiang.mu@letote.cn>
2020-05-08 11:15:41 +0800

    使用系统内置统计指标名称 自定义指标值

--- telemetry-code-along/lib/quantum/telemetry.ex
+++ telemetry-code-along/lib/quantum/telemetry.ex
@@ -24,11 +24,20 @@ defmodule Quantum.Telemetry do
         "phoenix.router_dispatch.stop.count",
         tag_values: &__MODULE__.endpoint_metadata/1,
         tags: [:plug, :plug_opts, :status]
+      ),
+      counter(
+        "phoenix.error_rendered.count",
+        tag_values: &__MODULE__.error_rendered_metadata/1,
+        tags: [:status, :request_path]
       )
     ]
   end
 
   def endpoint_metadata(%{conn: %{status: status}, plug: plug, plug_opts: plug_opts}) do
     %{ status: status, plug: plug, plug_opts: plug_opts }
+  end
+
+  def error_rendered_metadata(%{conn: %{request_path: request_path}, status: status}) do
+    %{status: status, request_path: request_path}
   end
 end
