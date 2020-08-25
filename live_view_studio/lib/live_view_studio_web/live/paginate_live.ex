afe19c5804ba0553997402face405f9b60b99e8b
zhulinpinyu <lixiang.mu@letote.cn>
2020-08-25 15:59:01 +0800

    排序

--- live_view_studio/lib/live_view_studio_web/live/paginate_live.ex
+++ live_view_studio/lib/live_view_studio_web/live/paginate_live.ex
@@ -10,13 +10,16 @@ defmodule LiveViewStudioWeb.PaginateLive do
   def handle_params(params, _url, socket) do
     page = String.to_integer(params["page"] || "1")
     per_page = String.to_integer(params["per_page"] || "5")
+    sort_by = String.to_atom(params["sort_by"] || "id")
+    sort_order = String.to_atom(params["sort_order"] || "asc")
     paginate_options = %{page: page, per_page: per_page}
-    donations = Donations.list_donations(paginate: paginate_options)
+    sort_options = %{sort_by: sort_by, sort_order: sort_order}
+    donations = Donations.list_donations(paginate: paginate_options, sort: sort_options)
 
     {:noreply,
      assign(socket,
        donations: donations,
-       options: paginate_options
+       options: Map.merge(paginate_options, sort_options)
      )}
   end
 
@@ -56,4 +59,31 @@ defmodule LiveViewStudioWeb.PaginateLive do
       class: class
     )
   end
+
+  defp sort_link(socket, text, sort_by, opts) do
+    text =
+      if sort_by == opts.sort_by do
+        text <> emoji(opts.sort_order)
+      else
+        text
+      end
+
+    live_patch(text,
+      to:
+        Routes.live_path(
+          socket,
+          __MODULE__,
+          sort_by: sort_by,
+          sort_order: toggle_sort_order(opts.sort_order),
+          page: opts.page,
+          per_page: opts.per_page
+        )
+    )
+  end
+
+  defp toggle_sort_order(:asc), do: :desc
+  defp toggle_sort_order(:desc), do: :asc
+
+  defp emoji(:asc), do: "👇"
+  defp emoji(:desc), do: "👆"
 end
